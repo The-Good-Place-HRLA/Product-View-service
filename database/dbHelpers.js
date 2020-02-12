9423491af0003420e525faef193aa8a3060794a1
Brian Price <brianprice88@gmail.com>
2020-02-12 08:52:09 -0800

    "Add other CRUD opterations for database"

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -2,8 +2,17 @@ var Product = require('./schema.js');
 var db = require('./index.js')
 
 var helpers = {
-  get: () => Product.find({}),
-  getOne: (productId) => Product.find({productId})
+  getOne: (productId) => Product.find({ productId }),
+  getAll: () => Product.find({}),
+
+  createOne: (product) => Product.create(product),
+  createMany: (products) => Product.insertMany(products),
+
+  updateOne: (productInfo, updatedProductInfo) => Product.updateOne(productInfo, updatedProductInfo),
+
+  deleteOne: (productInfo) => Product.deleteOne(productInfo),
+  deleteAll: () => Product.deleteMany({}),
+
 }
 
 module.exports = helpers
