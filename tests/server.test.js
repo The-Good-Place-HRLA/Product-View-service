c680f8d5ef6bb5cb07ad81496067d702551c306a
Brian Price <brianprice88@gmail.com>
2020-02-12 11:02:55 -0800

    "Write all server endpoint tests"

--- tests/server.test.js
+++ tests/server.test.js
@@ -7,14 +7,42 @@ describe('server api', () => {
     it('responds to a request for all items', async (done) => {
         const response = await request.get('/api');
         expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBe(100);
         done();
     })
 
     it('gets responds to a request for one item', async (done) => {
         const response = await request.get('/api/4');
         expect(response.statusCode).toBe(200);
-        expect(response.body.length).toBe(1);
+        done();
+    })
+
+    it('gets responds to a request to create items', async (done) => {
+        const response = await request.post('/api');
+        expect(response.statusCode).toBe(201);
+        done();
+    })
+
+    it('gets responds to a request to update an item', async (done) => {
+        const response = await request.put('/api/4');
+        expect(response.statusCode).toBe(202);
+        done();
+    })
+
+    it('gets responds to a request to delete one item', async (done) => {
+        const response = await request.delete('/api/4');
+        expect(response.statusCode).toBe(203);
+        done();
+    })
+
+    it('gets responds to a request to delete all items', async (done) => {
+        const response = await request.delete('/api');
+        expect(response.statusCode).toBe(203);
+        done();
+    })
+
+    it('gets responds to a request for one item', async (done) => {
+        const response = await request.get('/api/4');
+        expect(response.statusCode).toBe(200);
         done();
     })
 })
