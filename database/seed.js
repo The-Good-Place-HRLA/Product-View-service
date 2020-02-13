38ed46208637639b1731d9c297997070a1869369
Brian Price <brianprice88@gmail.com>
2020-02-12 16:47:13 -0800

    "Set up data generator function and tested with increasing records for regular mongoose insertion.  It takes 18 seconds for 100000 records so need to optimize before trying 100* that."

--- database/seed.js
+++ database/seed.js
@@ -1,51 +1,81 @@
 const mongoose = require('mongoose');
 const db = require('./index.js');
 const Product = require('./schema.js')
-const seedData = require('./products.json');
+// const seedData = require('./products.json');
 const faker = require('faker');
 
-let products = [];
-let sizes = ["XS", "S", "M", "L", "XL"];
-let colors = ["Really Red", "Old Orange", "Yucky Yellow", "Googly Green", "Big Blue", "Pasty Purple", "Wiggly White", "Bloopy Black", "Marco Mustard"];
+var dataGenerator = () => {
+  var before = new Date();
+  var items = []
+  for (var i = 0; i < 100000; i++) {
+    var item = new Product({
+      productId: i,
+      name: faker.commerce.productName(),
+      brand: (["Rex Specs", "Ruffwear", "Metolius", "Nite Ize", "Portland Design Works", "Cycle Dog", "Big Agnes", "Eagle Creek"][Math.floor(Math.random() * 8)]),
+      item: faker.random.number({ min: 1, max: 200000 }),
+      color: faker.commerce.color(),
+      rating: (Math.random() * 5).toFixed(1),
+      price: faker.commerce.price(),
+      size: (["XS", "S", "M", "L", "XL"][Math.floor(Math.random() * 5)]),
+      images: [faker.image.imageUrl(), faker.image.imageUrl(), faker.image.imageUrl(), faker.image.imageUrl(), faker.image.imageUrl(), faker.image.imageUrl()],
+      description: `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${faker.commerce.product()}`,
+    });
 
-for (var i = 0; i < 3; i++) {
-  let realProduct = {};
-  realProduct.productId = i;
-  realProduct.name = seedData[i].name;
-  realProduct.brand = seedData[i].brand;
-  realProduct.item = seedData[i].item;
-  realProduct.color = seedData[i].color;
-  realProduct.rating = Math.floor(Math.random() * 100);
-  realProduct.price = seedData[i].price
-  realProduct.size = seedData[i].size;
-  realProduct.images = seedData[i].images;
-  realProduct.description = `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${faker.commerce.product()}`;
+    items.push(item);
 
-  products.push(realProduct);
+  }
+
+  Product.insertMany(items)
+  .then(() => console.log('finished in ' + (new Date() - before)))
+  .catch(err => console.error(err))
+
 }
 
-for (var i = 3; i < 100; i++) {
-  let product = {};
-  product.productId = i;
-  product.name = seedData[i].name;
-  product.brand = seedData[i].brand;
-  product.item = faker.random.number({min: 1, max:200000});
-  product.color = `${colors[Math.floor(Math.random() * colors.length - 1)]}/${colors[Math.floor(Math.random() * colors.length - 1)]}`;
-  product.rating = Math.floor(Math.random() * 100);
-  product.price = Math.floor(Math.random() * 500) + 0.95;
-  product.size = sizes[Math.floor(Math.random() * 5)];
-  product.images = seedData[i].images;
-  product.description = `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${faker.commerce.product()}`;
+dataGenerator();
 
-  products.push(product);
-}
 
-var insertSeedData = () => {
-  Product.create(products)
-  .then(() => {
-    console.log('database seeded');
-  })
-  .catch(err => console.error(err));
-};
+// let products = [];
+// let sizes = ["XS", "S", "M", "L", "XL"];
+// let colors = ["Really Red", "Old Orange", "Yucky Yellow", "Googly Green", "Big Blue", "Pasty Purple", "Wiggly White", "Bloopy Black", "Marco Mustard"];
 
-insertSeedData()
+// for (var i = 0; i < 3; i++) {
+//   let realProduct = {};
+//   realProduct.productId = i;
+//   realProduct.name = seedData[i].name;
+//   realProduct.brand = seedData[i].brand;
+//   realProduct.item = seedData[i].item;
+//   realProduct.color = seedData[i].color;
+//   realProduct.rating = Math.floor(Math.random() * 100);
+//   realProduct.price = seedData[i].price
+//   realProduct.size = seedData[i].size;
+//   realProduct.images = seedData[i].images;
+//   realProduct.description = `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${faker.commerce.product()}`;
+
+//   products.push(realProduct);
+// }
+
+// for (var i = 3; i < 100; i++) {
+//   let product = {};
+//   product.productId = i;
+//   product.name = seedData[i].name;
+//   product.brand = seedData[i].brand;
+//   product.item = faker.random.number({min: 1, max:200000});
+//   product.color = `${colors[Math.floor(Math.random() * colors.length - 1)]}/${colors[Math.floor(Math.random() * colors.length - 1)]}`;
+//   product.rating = Math.floor(Math.random() * 100);
+//   product.price = Math.floor(Math.random() * 500) + 0.95;
+//   product.size = sizes[Math.floor(Math.random() * 5)];
+//   product.images = seedData[i].images;
+//   product.description = `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${faker.commerce.product()}`;
+
+//   products.push(product);
+// }
+
+// var insertSeedData = () => {
+//   Product.create(products)
+//   .then(() => {
+//     console.log('database seeded');
+//   })
+//   .catch(err => console.error(err));
+// };
+
+// insertSeedData()
