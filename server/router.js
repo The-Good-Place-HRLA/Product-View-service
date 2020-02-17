64076a99a87d6eccae70ccbbd5c4129280ffd283
Brian Price <brianprice88@gmail.com>
2020-02-17 10:37:13 -0800

    "Add routes for queries with postgres"

--- server/router.js
+++ server/router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const controllers = require('./controllers.js');
+const pgcontrollers = require('./pgcontrollers.js')
 
 router
   .route('/api')
@@ -20,5 +21,17 @@ router
   router
   .route('/api/getAll/:name')
   .get(controllers.getAllbyName)
+
+  router
+  .route('/pg/:productId')
+  .get(pgcontrollers.getById)
+
+  router
+  .route('/pg/:name')
+  .get(pgcontrollers.getFirstByName)
+
+  router
+  .route('/pg/getAll/:name')
+  .get(pgcontrollers.getByName)
 
 module.exports = router;
