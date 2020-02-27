55a5c6a748f0d4b4373a98b2a00ef1913fe9c9e6
Brian Price <brianprice88@gmail.com>
2020-02-27 11:04:47 -0800

    "Add bundle compression with brotli and gzip"

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 const path = require('path');
 const webpack = require('webpack');
+var BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
 
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
+  plugins: [
+    new BrotliGzipPlugin({
+        asset: '[path].br[query]',
+        algorithm: 'brotli',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8,
+        quality: 11
+    }),
+    new BrotliGzipPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.(js|css|html|svg)$/,
+        threshold: 10240,
+        minRatio: 0.8
+    })
+],
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'KLbundle.js',
